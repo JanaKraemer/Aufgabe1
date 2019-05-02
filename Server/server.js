f9aba4f59124279cadf1afdafe92b3a5bc4e2358
JanaKraemer <kraemerjana@yahoo.de>
2019-05-02 12:55:57 +0200

    server

---
+++ Server/server.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Http = require("http"); // Ein http Objekt wird im Code generiert//
+var L05_Server;
+(function (L05_Server) {
+    console.log("Starting server"); //Starting server wird in der Konsole ausgegeben.//
+    let port = Number(process.env.PORT); // Eine Variable wird deklariert, vom Typ number. Die Umgebungsvariable port sagt dem Server worauf er achten muss.//
+    if (!port) // Wenn Port nicht gefunden wird, dann wird dem Port eine Zahl zugewiesen.//
+        port = 8100; // Die Variable bekommt eine Zahl zugewiesen.//
+    let server = Http.createServer(); //Ein Server wird generiert//
+    server.addListener("request", handleRequest); // Der Server bekommt den Event-Listener "request"//
+    server.addListener("listening", handleListen); // Der Server bekommt den Event-Listener "listening"//
+    server.listen(port); // Der Server überwacht den Port//
+    function handleListen() {
+        console.log("Listening"); //Listening wird  auf der Konsole aufegegeben.//
+    }
+    function handleRequest(_request, _response) {
+        console.log("I hear voices!"); // I hear voices! wird auf der Konsole ausgegeben.//
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // .setHeader liest einen Header aus, der sich in einer Warteschlange befindet und noch nicht an den Client gesendet wurde.//
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // .setHeader liest einen Header aus, der sich in einer Warteschlange befindet und noch nicht an den Client gesendet wurde. Der Befehl gibt die Erlaubnis, alles auszulesen.//
+        _response.write(_request.url); // Infos werden an den Client übergeben und in die URL geschriben//
+        _response.end(); // Diese Methode signalisiert dem Server, dass alle Antworten gesendet wurden und die Nachricht vollständig ist.//
+    }
+})(L05_Server || (L05_Server = {}));
+//# sourceMappingURL=server.js.map
