9da883d6e51c45efe4ca8ada4e4bf2009645af6f
JanaKraemer <kraemerjana@yahoo.de>
2019-05-12 12:18:04 +0200

    fertigstellung

--- eis_response/data.js
+++ eis_response/data.js
@@ -16,6 +16,10 @@ var response;
             { type: "checkbox", name: "Streußel", value: "0", id: "check2.1", class: "Streußel", step: "0", min: "0", max: "0", alt: "0.5" },
             { type: "checkbox", name: "Schoko-Streußel", value: "0", id: "check3.1", class: "Schoko-Streußel", step: "0", min: "0", max: "0", alt: "0.5" },
             { type: "checkbox", name: "Erdbeer-Soße", value: "0", id: "check4.1", class: "Erdbeer-Soße", step: "0", min: "0", max: "0", alt: "0.5" },
+        ],
+        "Zustellung": [
+            { type: "radio", name: "Zustellung", value: "0", id: "radio1", class: "Lieferung", step: "0", min: "0", max: "0", alt: "2" },
+            { type: "radio", name: "Zustellung", value: "0", id: "radio1", class: "Abholung", step: "0", min: "0", max: "0", alt: "0" },
         ]
     };
 })(response || (response = {}));
